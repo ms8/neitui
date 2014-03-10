94df7e1d12bb69ef43508db9593cf51e97a0b4f3
ms8 <woloveni8@126.com>
2014-03-10 22:21:33 +0800

    首页样式修改

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,7 +6,7 @@
 <section class="pad-top-25" id="our-services">
     <div class="container">
         <div style="visibility: visible; opacity: 1;" class="sort_by_cat" id="filters" data-option-key="filter">
-            <div id="filter-up"></div>
+            <span id="filter-up">公司所在城市：</span>
             <a href="#filter" data-option-value="*" class="active_sort">北京</a>
             <span class="text-sep">/</span>
             <a class="" href="#filter" data-option-value=".css_sort">上海</a>
@@ -32,6 +32,29 @@
                     </a>
                 </li>
             <?php }?>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/1.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/2.jpg" /><div class="pe-description"><h3>hopeful</h3><p>Abridged the circuit of his hopeful days</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/3.jpg" /><div class="pe-description"><h3>virtue</h3><p>Whiles both his youth and virtue did intend</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/4.jpg" /><div class="pe-description"><h3>endeavors</h3><p>The good endeavors of deserving praise</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/5.jpg" /><div class="pe-description"><h3>monument</h3><p>What memorable monument can last</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/6.jpg" /><div class="pe-description"><h3>name</h3><p>Whereon to build his never-blemished name</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/7.jpg" /><div class="pe-description"><h3>life was graced</h3><p>But his own worth, wherein his life was graced</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/8.jpg" /><div class="pe-description"><h3>the same</h3><p>Sith as that ever he maintained the same?</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/9.jpg" /><div class="pe-description"><h3>oblivion</h3><p>Oblivion in the darkest day to come</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/10.jpg" /><div class="pe-description"><h3>sin shall tread</h3><p>When sin shall tread on merit in the dust</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/11.jpg" /><div class="pe-description"><h3>lamentable tomb</h3><p>Cannot rase out the lamentable tomb</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/12.jpg" /><div class="pe-description"><h3>short-lived deserts</h3><p>Of his short-lived deserts; but still they must</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/13.jpg" /><div class="pe-description"><h3>hearts and memories</h3><p>Even in the hearts and memories of men</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/14.jpg" /><div class="pe-description"><h3>respect</h3><p>Claim fit respect, that they, in every limb</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/15.jpg" /><div class="pe-description"><h3>comfort</h3><p>Remembering what he was, with comfort then</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/16.jpg" /><div class="pe-description"><h3>pattern</h3><p>May pattern out one truly good, by him</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/17.jpg" /><div class="pe-description"><h3>truly good</h3><p>For he was truly good, if honest care</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/18.jpg" /><div class="pe-description"><h3>harmless conversation</h3><p>Of harmless conversation may commend</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/19.jpg" /><div class="pe-description"><h3>recompensed</h3><p>Ill recompensed only in his end</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/20.jpg" /><div class="pe-description"><h3>tongue</h3><p>Nor can the tongue of him who loved him least</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/21.jpg" /><div class="pe-description"><h3>superlative</h3><p>To one superlative above the rest</p></div></a></li>
+            <li><a href="#"><img src="<?php echo Yii::app()->baseUrl.CSS_PATH?>images/thumbs/22.jpg" /><div class="pe-description"><h3>steady faith</h3><p>Of many men in steady faith reprove</p></div></a></li>
+
         </ul>
     </div>
 </section>
@@ -151,4 +174,171 @@
             }
         });
     };
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.register_radio li input').click(function(e){
+            $(this).parent('li').addClass('current').append('<em></em>').siblings().removeClass('current').find('em').remove();
+        });
+    });
+
+    $(function() {
+
+        var Photo	= (function() {
+
+            // list of thumbs
+            var $list		= $('#pe-thumbs'),
+            // list's width and offset left.
+            // this will be used to know the position of the description container
+                listW		= $list.width(),
+                listL		= $list.offset().left,
+            // the images
+                $elems		= $list.find('img'),
+            // the description containers
+                $descrp		= $list.find('div.pe-description'),
+            // maxScale : maximum scale value the image will have
+            // minOpacity / maxOpacity : minimum (set in the CSS) and maximum values for the image's opacity
+                settings	= {
+                    maxScale	: 1.3,
+                    maxOpacity	: 0.9,
+                    minOpacity	: 0.5
+//                    minOpacity	: Number( $elems.css('opacity') )
+                },
+                init		= function() {
+
+                    // minScale will be set in the CSS
+                    settings.minScale = _getScaleVal() || 1;
+                    // preload the images (thumbs)
+                    _loadImages( function() {
+
+                        _calcDescrp();
+                        _initEvents();
+
+                    });
+
+                },
+            // Get Value of CSS Scale through JavaScript:
+            // http://css-tricks.com/get-value-of-css-rotation-through-javascript/
+                _getScaleVal= function() {
+
+                    var st = window.getComputedStyle($elems.get(0), null),
+                        tr = st.getPropertyValue("-webkit-transform") ||
+                            st.getPropertyValue("-moz-transform") ||
+                            st.getPropertyValue("-ms-transform") ||
+                            st.getPropertyValue("-o-transform") ||
+                            st.getPropertyValue("transform") ||
+                            "fail...";
+
+                    if( tr !== 'none' ) {
+
+                        var values = tr.split('(')[1].split(')')[0].split(','),
+                            a = values[0],
+                            b = values[1],
+                            c = values[2],
+                            d = values[3];
+
+                        return Math.sqrt( a * a + b * b );
+
+                    }
+
+                },
+            // calculates the style values for the description containers,
+            // based on the settings variable
+                _calcDescrp	= function() {
+
+                    $descrp.each( function(i) {
+
+                        var $el		= $(this),
+                            $img	= $el.prev(),
+                            img_w	= $img.width(),
+                            img_h	= $img.height(),
+                            img_n_w	= settings.maxScale * img_w,
+                            img_n_h	= settings.maxScale * img_h,
+                            space_t = ( img_n_h - img_h ) / 2,
+                            space_l = ( img_n_w - img_w ) / 2;
+
+                        $el.data( 'space_l', space_l ).css({
+                            height	: settings.maxScale * $el.height(),
+                            top		: -space_t,
+                            left	: img_n_w - space_l
+                        });
+
+                    });
+
+                },
+                _initEvents	= function() {
+
+                    $elems.on('proximity.Photo', { max: 80, throttle: 10, fireOutOfBounds : true }, function(event, proximity, distance) {
+
+                        var $el			= $(this),
+                            $li			= $el.closest('li'),
+                            $desc		= $el.next(),
+                            scaleVal	= proximity * ( settings.maxScale - settings.minScale ) + settings.minScale,
+                            scaleExp	= 'scale(' + scaleVal + ')';
+
+                        // change the z-index of the element once it reaches the maximum scale value
+                        // also, show the description container
+                        if( scaleVal === settings.maxScale ) {
+
+                            $li.css( 'z-index', 100 );
+
+                            if( $desc.offset().left + $desc.width() > listL + listW ) {
+
+                                $desc.css( 'left', -$desc.width() - $desc.data( 'space_l' ) );
+
+                            }
+
+                            $desc.fadeIn( 800 );
+
+                        }
+                        else {
+
+                            $li.css( 'z-index', 1 );
+
+                            $desc.stop(true,true).hide();
+
+                        }
+
+                        $el.css({
+                            '-webkit-transform'	: scaleExp,
+                            '-moz-transform'	: scaleExp,
+                            '-o-transform'		: scaleExp,
+                            '-ms-transform'		: scaleExp,
+                            'transform'			: scaleExp,
+                            'opacity'			: ( proximity * ( settings.maxOpacity - settings.minOpacity ) + settings.minOpacity )
+                        });
+
+                    });
+
+                },
+                _loadImages	= function( callback ) {
+
+                    var loaded 	= 0,
+                        total	= $elems.length;
+
+                    $elems.each( function(i) {
+
+                        var $el = $(this);
+
+                        $('<img/>').load( function() {
+
+                            ++loaded;
+                            if( loaded === total )
+                                callback.call();
+
+                        }).attr( 'src', $el.attr('src') );
+
+                    });
+
+                };
+
+            return {
+                init	: init
+            };
+
+        })();
+
+        Photo.init();
+
+    });
 </script>
