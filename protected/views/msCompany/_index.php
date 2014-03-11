2c8eb1f0940b268e1c385001a52477fd68630a66
ms8 <woloveni8@126.com>
2014-03-11 23:37:58 +0800

    公司列表

---
+++ protected/views/msCompany/_index.php
@@ -0,0 +1,24 @@
+<?php
+/* @var $this MsCompanyController */
+/* @var $data MsCompany */
+?>
+
+<div class="col-sm-6 col-md-3 team-member-wrap">
+    <div class="team-member">
+        <div class="member-thumb row">
+            <div class="col-md-6">
+                <h5 class="member-name"><?php echo CHtml::encode($data->name); ?></h5>
+            </div>
+            <div class="col-md-6">
+                <a href="<?php echo Yii::app()->baseUrl.'/mscompany/view/'.$data->id?>"><img alt="Member Image" class="img-responsive img-polaroid" src="<?php echo Yii::app()->baseUrl.'/'.$data->logo?>"></a>
+            </div>
+        </div>
+        <div class="member-details">
+            <span class="position">【吸引力】 <?php echo CHtml::encode($data->tags); ?></span>
+            <p>【公司介绍】 <?php echo CHtml::encode($data->description); ?></p>
+            <p>【招聘职位】 <?php echo CHtml::encode($data->description); ?></p>
+            <p class="text-center"><a class="btn btn-flat flat-primary" href="<?php echo Yii::app()->baseUrl.'/mscompany/view/'.$data->id?>">投简历</a></p>
+        </div>
+    </div>
+
+</div>
