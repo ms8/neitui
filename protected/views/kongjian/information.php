22d5290736439027cd256dab90a3cc1e7b0f7970
syd3050 <syd3050@163.com>
2014-03-16 22:04:12 +0800

    个人信息页面

--- protected/views/kongjian/information.php
+++ protected/views/kongjian/information.php
@@ -1,82 +1,108 @@
 <section class="pad-25" id="action-box">
     <div class="container">
+        <div class="alert alert-success alert-dismissable">
+            <strong>信息维护</strong>
+            &nbsp;&nbsp;完善你的信息，让公司全都找到你~
+        </div>
         <div class="action-box">
+
             <?php
             $form=$this->beginWidget('CActiveForm', array(
                 'id'=>'ms-students-form',
                 'enableAjaxValidation'=>false,
-                'action'=>Yii::app()->createUrl("/kongjian/information")
+                'action'=>Yii::app()->createUrl("/kongjian/information"),
+                'htmlOptions'=>array('class'=>'form-horizontal')
             ));
             ?>
 
             <?php echo $form->errorSummary($model); ?>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'realname'); ?>
-                <?php echo $form->textField($model,'realname',array('size'=>50,'maxlength'=>50)); ?>
-                <?php echo $form->error($model,'realname'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'realname',array('class'=>'col-sm-2 control-label')); ?>
+                <div class="col-sm-10">
+                    <?php echo $form->textField($model,'realname',array('size'=>50,'maxlength'=>50,
+                        'class'=>'form-control')); ?>
+                    <?php echo $form->error($model,'realname'); ?>
+                </div>
+
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'phone'); ?>
-                <?php echo $form->textField($model,'phone',array('size'=>20,'maxlength'=>20)); ?>
-                <?php echo $form->error($model,'phone'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'phone',array('class'=>'col-sm-2 control-label')); ?>
+                <div class="col-sm-10">
+                    <?php echo $form->textField($model,'phone',array('size'=>20,'maxlength'=>20,
+                        'class'=>'form-control')); ?>
+                    <?php echo $form->error($model,'phone'); ?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'universitytype'); ?>
-                <?php echo $form->radioButtonList($model,'universitytype',$uniarr,
-                    array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'radiolabel')) )?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'universitytype',array('class'=>'col-sm-2 control-label')); ?>
+                <div style="margin-top:7px">
+                    <?php echo $form->radioButtonList($model,'universitytype',$uniarr,
+                        array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'')) )?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'universityname'); ?>
-                <?php echo $form->textField($model,'universityname',array('size'=>50,'maxlength'=>50)); ?>
-                <?php echo $form->error($model,'universityname'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'universityname',array('class'=>'col-sm-2 control-label')); ?>
+                <div class="col-sm-10">
+                    <?php echo $form->textField($model,'universityname',array('size'=>50,'maxlength'=>50,
+                        'class'=>'form-control')); ?>
+                    <?php echo $form->error($model,'universityname'); ?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'degree'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'degree',array('class'=>'col-sm-2 control-label')); ?>
+                <div style="margin-top:7px">
                 <?php echo $form->radioButtonList($model,'degree',$degreearr,
-                    array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'radiolabel')) )?>
+                    array('separator'=>'&nbsp;','labelOptions'=>array()) )?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'jianglitype'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'jianglitype',array('class'=>'col-sm-2 control-label')); ?>
+                <div style="margin-top:7px">
                 <?php echo $form->radioButtonList($model,'jianglitype',$jiangliarr,
                     array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'radiolabel')) )?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'sex'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'sex',array('class'=>'col-sm-2 control-label')); ?>
+                <div style="margin-top:7px">
                 <?php echo $form->radioButtonList($model,'sex', array('1'=>'男','0'=>'女'),
                     array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'radiolabel')) )?>
+                </div>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'projects'); ?>
-                <?php echo $form->textArea($model,'projects',array('rows'=>10, 'cols'=>100)); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'projects',array('class'=>'col-sm-2 control-label')); ?>
+                <?php echo $form->textArea($model,'projects',array('rows'=>7, 'cols'=>70,'style'=>'width:81%','class'=>'form-control')); ?>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'skill'); ?>
-                <?php echo $form->textArea($model,'skill',array('rows'=>10, 'cols'=>100)); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'skill',array('class'=>'col-sm-2 control-label')); ?>
+                <?php echo $form->textArea($model,'skill',array('rows'=>7, 'cols'=>70,'style'=>'width:81%','class'=>'form-control')); ?>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'peixun'); ?>
-                <?php echo $form->textArea($model,'peixun',array('rows'=>10, 'cols'=>100)); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'peixun',array('class'=>'col-sm-2 control-label')); ?>
+                <?php echo $form->textArea($model,'peixun',array('rows'=>7, 'cols'=>70,'style'=>'width:81%','class'=>'form-control')); ?>
             </div>
 
-            <div class="row">
-                <?php echo $form->labelEx($model,'hasoffer'); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'hasoffer',array('class'=>'col-sm-2 control-label')); ?>
+                <div style="margin-top:7px">
                 <?php echo $form->radioButtonList($model,'hasoffer', array('1'=>'是','0'=>'否'),
                     array('separator'=>'&nbsp;','labelOptions'=>array('class'=>'radiolabel')) )?>
+                </div>
             </div>
 
 
-            <div class="row buttons">
-                <?php echo CHtml::submitButton($model->isNewRecord ? '保存' : '保存'); ?>
+            <div class="col-sm-10">
+                <input type="submit" value="保存" name="yt0" class="btn btn-flat flat-color">
             </div>
 
             <?php $this->endWidget(); ?>
