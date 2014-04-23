ae071bdaf0809a497d59747998a75016435238ff
syd3050 <syd3050@163.com>
2014-04-23 14:46:36 +0800

    php发送http请求

--- protected/components/HttpSender.php
+++ protected/components/HttpSender.php
@@ -10,9 +10,12 @@
 class HttpSender {
     function sock_get($url)
     {
-        $info = parse_url($url);
-        $fp = fsockopen($info["host"], $info["port"], $errno, $errstr, 3);
 
+        $info = parse_url($url);
+
+        $socket = socket_create(2, 1, 6) ;
+
+        $connection = socket_connect($socket, $info["host"], $info["port"]) or die(CJSON::encode(array('status'=>"Could not connet\n")));
         $head = "GET ".$info['path']." HTTP/1.1\r\n";
         $head .= "Host: ".$info['host'].":".$info["port"]."\r\n";
         $head.= "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
@@ -20,7 +23,17 @@ class HttpSender {
         $head.="Accept-Language: zh-CN,zh;q=0.8\r\n";
         $head.="Accept-Charset: GBK,utf-8;q=0.7,*;q=0.3\r\n";
         $head .= "\r\n";
-        $write = fputs($fp, $head);
-        fclose($fp);
+
+
+        $response="";
+        socket_write($socket, $head) or die(CJSON::encode(array('status'=>"Write failed")));
+        $i=1;
+        while ($i>0) {
+            $buff = socket_read($socket, 1024, 1);
+            $response.=$buff."\n";
+            $i--;
+
+        }
+        socket_close($socket);
     }
 }
