24f49d2979864fb673f64c5232a53a2338100248
syd3050 <syd3050@163.com>
2014-04-30 23:08:21 +0800

    个人中心密码修改

--- protected/views/msCompany/changepwd.php
+++ protected/views/msCompany/changepwd.php
@@ -1,35 +1,43 @@
-<section class="pad-25" id="action-box" xmlns="http://www.w3.org/1999/html">
+<section class="pad-25">
     <div class="container">
-        <div class="subpage-title noline">
-            <h5>修改密码</h5>
-        </div>
-        <div class="action-box">
-            <form name="resetPassword"  id="resetPassword" class="form-horizontal" action="<?php echo Yii::app()->createUrl('site/directReset');?>">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">新密码:</label>
-                    <div class="col-sm-5">
-                        <input name="password" class="form-control" type="password" placeholder="输入新密码">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="widget">
+                    <div class="subpage-title noline">
+                        <h5>修改密码</h5>
                     </div>
-                    <label  class="col-sm-4  errorMessage text-left" error-message="password">
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">确认新密码:</label>
-                    <div class="col-sm-5">
-                        <input name="password_confirm" class="form-control" type="password" placeholder="确认新密码">
+                    <div style="padding:20px">
+                        <form name="resetPassword"  id="resetPassword" class="form-horizontal" action="<?php echo Yii::app()->createUrl('site/directReset');?>">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">新密码:</label>
+                                <div class="col-sm-5">
+                                    <input name="password" class="form-control" type="password" placeholder="输入新密码">
+                                </div>
+                                <label  class="col-sm-4  errorMessage text-left" error-message="password">
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">确认新密码:</label>
+                                <div class="col-sm-5">
+                                    <input name="password_confirm" class="form-control" type="password" placeholder="确认新密码">
+                                </div>
+                                <label  class="col-sm-4  errorMessage text-left" error-message="password_confirm">
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-4">
+                                    <button style="margin-top: 5px" id="resetPwd" class="btn btn-flat flat-color"  type="button">重置密码</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <label  class="col-sm-4  errorMessage text-left" error-message="password_confirm">
-                    </label>
                 </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-4">
-                        <button style="margin-top: 5px" id="resetPwd" class="btn btn-flat flat-color"  type="button">重置密码</button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </section>
+
+
 <script type="text/javascript">
     $(function(){
         //菜单选中公司
