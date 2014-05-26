ac07e3f346b4c8c01f1aab13063b7cd1244576c6
syd3050 <syd3050@163.com>
2014-05-26 23:14:33 +0800

    后台管理登陆页面、简历管理功能修改

--- protected/modules/srbac/views/msCompany/admin.php
+++ protected/modules/srbac/views/msCompany/admin.php
@@ -2,61 +2,41 @@
 /* @var $this MsCompanyController */
 /* @var $model MsCompany */
 
-$this->breadcrumbs=array(
-	'Ms Companies'=>array('index'),
-	'Manage',
-);
-
-//$this->menu=array(
-//	array('label'=>'List MsCompany', 'url'=>array('index')),
-//	array('label'=>'Create MsCompany', 'url'=>array('create')),
-//);
 ?>
 
 <table class="table">
         <tr>
-            <td>ID</td>
             <td>名称</td>
-            <td>主页</td>
             <td>Email</td>
-            <td>地址</td>
+            <td>主页</td>
+            <td>积分</td>
+            <td>创建时间</td>
             <td colspan="3">操作</td>
         </tr>
 
-<?php $this->widget('zii.widgets.CListView', array(
-    'dataProvider'=>$dataProvider,
-    'itemView'=>'_index',
-    "itemsCssClass"=>"row items",
-    'template'=>'<div class="list">{items}</div>{pager}',
-    'pager'=>array(
-        'class'=>'CLinkPager',
-        'cssFile'=>"pagination",
-        'htmlOptions'=>array('class'=>'pagination'),
-        'selectedPageCssClass'=>'active',
-        'hiddenPageCssClass'=>'disabled',
-    ),
-)); ?>
-
+        <?php foreach($companys as $company){?>
+            <tr>
+                <td>
+                    <a target="_blank" href="<?php echo Yii::app()->baseUrl.'/mscompany/view/'.$company['id'] ?>">
+                        <?php echo $company['name']?>
+                    </a>
+                 </td>
+                <td><?php echo $company['account']?></td>
+                <td><?php echo $company['website']?></td>
+                <td><?php echo $company['score']?></td>
+                <td><?php echo $company['createtime']?></td>
+                <td><a href="#" onclick="deleteCompany(<?php echo $company['id']?>)">删除</a></td>
+                <td><a href="#" onclick="weightCompany(<?php echo $company['id']?>)">上首页</a></td>
+            </tr>
+        <?php }?>
 </table>
 
-<?php
-//$this->widget('zii.widgets.grid.CGridView', array(
-//	'id'=>'ms-company-grid',
-//	'dataProvider'=>$model->search(),
-//	'filter'=>$model,
-//	'columns'=>array(
-//		'id',
-//		'name',
-//		'website',
-//		'email',
-//		'address',
-//		'account',
-//		array(
-//			'class'=>'CButtonColumn',
-//		),
-//	),
-//));
-?>
+<div class="text-center" >
+    <?php
+    //分页widget代码:
+    $this->widget('CLinkPager',array('pages'=>$pages,'selectedPageCssClass'=>'active','hiddenPageCssClass'=>'disabled', 'htmlOptions'=>array('class'=>'pagination')));
+    ?>
+</div>
 
 <script type="text/javascript">
     function deleteCompany(cid){
